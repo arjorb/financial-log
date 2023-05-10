a97ccc6cb96b8caf2d9bda8dcee5ad864109bf88
John U <arjorb@gmail.com>
2023-05-10 11:33:30 +0200

    add the class folder and export and import it into the app file

--- public/app.js
+++ public/app.js
@@ -1,4 +1,12 @@
-"use strict";
+import { Invoice } from './classes/Invoice.js';
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.amount);
+});
 const form = document.querySelector('.new-item-form');
 const type = document.querySelector('#type');
 const toForm = document.querySelector('#tofrom');
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+import {Invoice} from './classes/Invoice.js'
+
+const invOne = new Invoice('mario','work on the mario website',250);
+const invTwo = new Invoice('luigi','work on the luigi website',300);
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invoices.forEach(inv =>{
+    console.log(inv.client, inv.amount, inv.amount)
+})
+
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 const type = document.querySelector('#type') as HTMLSelectElement;
 const toForm = document.querySelector('#tofrom') as HTMLInputElement;
